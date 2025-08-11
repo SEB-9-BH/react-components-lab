283e08ece3cb56c389bbec05b46572d4cc520b2c
abdulla <alshaikhabdulla6@gmail.com>
2025-08-11 13:29:26 +0300

    completed react-components lab

---
+++ react-components-lab/src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import "./WeatherForecast.css"
+
+const WeatherForecast = ({ day, img, imgAlt, condition, time }) => {
+    return (
+        <div className="weather">
+            <h2>{day}</h2>
+            <img src={img} alt={imgAlt} />
+            <p><span>conditions: </span> {condition} </p>
+            <p><span>time: </span> {time}</p>
+        </div>
+    )
+}
+
+export default WeatherForecast
